67c93456eeb3e81ea76fa95ab93e1ddc43367ddf
shikha <69755719+creater-shikha@users.noreply.github.com>
2022-03-21 01:30:27 +0530

    initial comit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Zoom from '@material-ui/core/Zoom';
+import Fab from '@material-ui/core/Fab';
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  const[rowNo,setrow]=useState(false);
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+  function openBox(){
+   
+   
+    setrow(true);
+
+    return
+  }
+  
+  return (
+    <div>
+      <form className="create-note">
+      {rowNo &&(
+        <input 
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />)}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          onClick={openBox}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={rowNo?3:1}
+        />
+        <Zoom in={rowNo}>
+        <Fab onClick={submitNote}><AddIcon/></Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh';
+function Header() {
+  return (
+    <header>
+      <h1><BrightnessHighIcon/> Important Notes</h1>
+    </header>
+  );
+}
+
+export default Header;
